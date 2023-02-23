284581e671bf501206bac48653ea7d01dba4e1f9
Vombit <45901675+Vombit@users.noreply.github.com>
2023-02-23 14:03:09 +0500

    update

--- main_app/models.py
+++ main_app/models.py
@@ -10,16 +10,32 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     currency = models.CharField(max_length=5, choices=cur, default='₽')
 
+    def __str__(self):
+        return self.name
+    
 class Discount(models.Model):
     code = models.CharField(max_length=30)
     discount = models.DecimalField(max_digits=6, decimal_places=2)
 
+    def __str__(self):
+        return self.code
+    
 class Tax(models.Model):
     name = models.CharField(max_length=100)
     tax = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     items = models.ManyToManyField(Item)
     discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True, blank=True)
     tax = models.ForeignKey('Tax', on_delete=models.SET_NULL, null=True, blank=True)
     total = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        total_price = 0
+        for item in self.items.all():
+            total_price += item.price
+        self.total =  total_price
+        super(Order, self).save(*args, **kwargs)
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from main_app.models import *
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def buy(request, id):
     item = Item.objects.get(id = id)
-
     session = stripe.checkout.Session.create(
         payment_method_types=['card'],
         line_items=[{
@@ -25,6 +25,7 @@ def buy(request, id):
         success_url = 'http://localhost:8000/success/',
         cancel_url = 'http://localhost:8000/cancel/',
     )
+    print(session.id)
     return JsonResponse({'id': session.id})
 
 def item(request, id):
@@ -36,7 +37,40 @@ def item(request, id):
     return render(request, 'main_app/item.html', context)
 
 
-def order(request, id):
+
+def create_payment_intent(amount, payment_method_id):
+    print(payment_method_id)
+    intent = stripe.PaymentIntent.create(
+        amount=int(amount * 100),
+        currency='usd',
+        payment_method=payment_method_id,
+        payment_method_types=['card'],
+        payment_method_options={
+            'card': {
+                'request_three_d_secure': 'any'
+            }
+        },
+        metadata={
+            'integration_check': 'accept_a_payment'
+        },
+        confirm=True
+    )
+    return intent.client_secret
+
+
+@csrf_exempt
+def payment_intent(request):
+    if request.method == 'GET':
+        return render(request, 'main_app/chekout.html')
+    if request.method == 'POST':
+        amount = int(request.POST['amount'])
+        payment_method = int(request.POST['payment_method_id'])
+        client_secret = create_payment_intent(amount, payment_method)
+        return JsonResponse({'client_secret': client_secret})
+
+
+
+def order(request):
     item = Item.objects.all()
 
     context = {
